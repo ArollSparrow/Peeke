a549987857b86b848903cea45ff5bb354b5bfd92
ArollSparrow <jkiaimorogo@gmail.com>
2026-02-16 21:28:40 +0300

    Delete lib/services/database_service_stub.dart

--- lib/services/database_service_stub.dart
+++
@@ -1,6 +0,0 @@
-// lib/services/database_service_stub.dart
-// Called on Android/iOS - database init handled in main() directly
-
-Future<void> initWebDatabase() async {
-  // No-op on mobile - sqflite handles it natively
-}
