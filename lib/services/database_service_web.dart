8cccce543780aec86816f38cdfee22ce20a4e1b2
ArollSparrow <jkiaimorogo@gmail.com>
2026-02-16 21:29:16 +0300

    Delete lib/services/database_service_web.dart

--- lib/services/database_service_web.dart
+++
@@ -1,9 +0,0 @@
-// lib/services/database_service_web.dart
-// Called ONLY when running in browser
-
-import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
-import 'package:sqflite/sqflite.dart';
-
-Future<void> initWebDatabase() async {
-  databaseFactory = databaseFactoryFfiWeb;
-}
