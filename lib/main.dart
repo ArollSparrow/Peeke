4862b80a84afc5a3bc70d347810d6753a05dc445
ArollSparrow <jkiaimorogo@gmail.com>
2026-02-26 11:33:47 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -107,6 +107,7 @@ class _PeekAppState extends State<PeekApp> with WidgetsBindingObserver {
           ),
         ),
 
+        // Classic CardTheme - the version that worked in your successful deployments
         cardTheme: CardTheme(
           color: const Color(0xFF1E1E1E),
           elevation: 4,
